c4a4a4f29f5d3f4ac2091cd47da4f6a0505a628a
Raju Dey <106087058+Rajuaiub009@users.noreply.github.com>
2022-07-05 22:07:44 +0600

    Delete MID_TERM_PROJECT directory

--- MID_TERM_PROJECT/Controller/footer.php
+++
@@ -1,8 +0,0 @@
-
-<html>
-    <body>
-
-<p> Admin <a href="https://github.com/Rajuaiub009"> Raju Dey </a>  </p>
-
-</body>
-</html>
--- MID_TERM_PROJECT/View/contactus.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE HTML>
-<html>
-    <body>
-        <form action = "profile.php">
-        <p> Email : rr@gmail.com </p>
-        <p> Mobile : 0171111111 </p>
-</body>
-</html>
