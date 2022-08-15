60fe6bccc0a8b1cf45a797f4cc6066a4c964e8f4
Raju Dey <106087058+Rajuaiub009@users.noreply.github.com>
2022-08-15 14:49:09 +0600

    Delete Lab_Task5 directory

--- Lab_Task5/View/nav.php
+++
@@ -1,5 +0,0 @@
-<ul>
-        <a href="addSeller.php"><button> Add Seller </button></a></li>
-        <a href="showAllSellers.php"> <button>Show all sellers </button></a></li>
-        <a href="searchSeller.php"> <button>Search Sellers</button></a></li>
-    </ul>
--- Lab_Task5/controller/deleteSeller.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-require_once '../Model/model.php';
-
-if (deleteSeller($_GET['id'])) {
-    header('..View/showAllSellers.php');
-}
-
- ?>
