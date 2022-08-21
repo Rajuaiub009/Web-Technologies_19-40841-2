e510e3dcb7498a1d8ac06c4d64a77e00862fd99d
Raju Dey <106087058+Rajuaiub009@users.noreply.github.com>
2022-08-21 21:43:09 +0600

    Lab_task9 Uploaded

---
+++ Lab_Task9/Livesearch.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+  <style>
+    h2{
+          background-color: darkblue;
+          text-align: center;
+          color: burlywood;
+           }
+      
+       body{
+        background-image: url("../images/img.jpg");
+          text-align: center;
+          color:white;
+         }
+  </style>
+<script>
+function showResult(str) {
+  if (str.length==0) {
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  xmlhttp.open("GET","../Controller/livesearch.php?q="+str,true);
+  xmlhttp.send();
+}
+</script>
+</head>
+<body>
+
+<form>
+  <br>
+  <h2> Search </h2>
+    <form action="../View/Profile.php"><input type="text" size="30" onkeyup="showResult(this.value)">
+<div id="livesearch"></div>
+</form>
+
+</body>
+</html>
